83943ce11e53b3e44e4adf6179137684b92bbc11
Jacob Hageman <jacob.hageman@nasa.gov>
2023-12-19 23:11:14 +0000

    Fix #104, Support generated config files

--- arch_build.cmake
+++ arch_build.cmake
@@ -21,13 +21,13 @@ set(LC_PLATFORM_CONFIG_FILE_LIST
 foreach(LC_CFGFILE ${LC_PLATFORM_CONFIG_FILE_LIST})
    get_filename_component(CFGKEY "${LC_CFGFILE}" NAME_WE) 
    if (DEFINED LC_CFGFILE_SRC_${CFGKEY}) 
-     set(DEFAULT_SOURCE "${LC_CFGFILE_SRC_${CFGKEY}}") 
+     set(DEFAULT_SOURCE GENERATED_FILE "${LC_CFGFILE_SRC_${CFGKEY}}")
    else() 
-     set(DEFAULT_SOURCE "${CMAKE_CURRENT_LIST_DIR}/config/default_${LC_CFGFILE}") 
+     set(DEFAULT_SOURCE FALLBACK_FILE "${CMAKE_CURRENT_LIST_DIR}/config/default_${LC_CFGFILE}")
    endif()
 
   generate_config_includefile(
     FILE_NAME           "${LC_CFGFILE}"
-    FALLBACK_FILE       "${DEFAULT_SOURCE}"
+    ${DEFAULT_SOURCE}
   )
 endforeach()
